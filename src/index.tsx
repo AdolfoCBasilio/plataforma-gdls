16386c4de88dc781f64eff54541c907f4088aeea
AdolfoCBasilio <adolfo_basiliogeniz@hotmail.com>
2022-11-29 18:17:30 -0600

    add Home and base components

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+//*Redux imports
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+
+//*Styles Sass
+import './styles/styles.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,30 @@
+//hooks
+import useMediaQuery from "../../hooks/useMediaQuery";
+import { useEffect, useState } from 'react';
+
+//components
+import { LayoutCardUser } from "../../components/ui/layoutCardUser";
+import { LayoutWeb } from '../../components/ui/layoutWeb';
+import Loader from "../../components/ui/loader/Loader";
+
+//styles
+import './_homepage.scss';
+
+export const HomePage = () => {
+    const [matches] = useMediaQuery("screen and (min-width: 769px)");
+    const [render, setRender] = useState(false);
+
+    const validateResponsive = () => matches ? <LayoutWeb /> : <LayoutCardUser />
+
+    useEffect(() => {
+        setTimeout(() => {
+            setRender(true)
+        }, 3000);
+    }, [])
+
+    return (
+        <div className="homepage__main-content" >
+            {render ? validateResponsive() : <Loader />}
+        </div>
+    )
+}
